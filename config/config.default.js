9730febd12c5b8d999117f37337666b91ad96238
nodejh <jianghangscu@gmail.com>
2019-10-12 16:21:19 +0800

    feat: init project

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-plugin-graphql default config
+ * @member Config#eggPluginGraphql
+ * @property {String} SOME_KEY - some description
+ */
+exports.eggPluginGraphql = {
+
+};
---
+++ test/fixtures/apps/egg-plugin-graphql-test/config/config.default.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.keys = '123456';
+
+exports.graphql = {
+  router: '/graphql',
+  // 是否加载到 app 上，默认开启
+  app: true,
+  // 是否加载到 agent 上，默认关闭
+  agent: false,
+  // 是否加载开发者工具 playground, 默认开启。路由同 router 字段。使用浏览器打开该可见
+  playground: true,
+};
+
+exports.middleware = [ 'graphql' ];
